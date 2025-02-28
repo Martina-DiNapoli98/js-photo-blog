39b888336ebd7f651b805f3f552093a370d842bd
Martina-dinapoli98 <martinadinapoli1998@gmail.com>
2025-02-28 16:04:33 +0100

    edit row

--- main.js
+++ main.js
@@ -5,21 +5,18 @@ fetch(cardsUrlEndpoint)
   .then(res => res.json())
   .then(cards => {
 
-
     console.log(cards);
 
     cards.forEach(card =>{
         let markup = renderCards(card)
-        cardEl.insertAdjacentHTML('beforeend', markup); 
-    
+        cardEl.insertAdjacentHTML('beforeend', markup);     
       })
-
 
   }).catch(err => console.error(err));
 
   function renderCards(cards){
     let markup = `
-     <div class="col-12 col-md-6 col-xl-4 my-2">
+                 <div class="col-12 col-md-6 col-xl-4 my-2 d-flex justify-content-center">
                     <div class="cards p-3">
                         <img src="${cards.url}" class="card-img-top" alt="" width="100px" height="200px">
                         <img src="./assets/img/pin.svg" alt="" width="20px" class="pin">
@@ -33,9 +30,8 @@ fetch(cardsUrlEndpoint)
                         </div>
                       </div>
                 </div>`
-                console.log(markup);
-                return markup
-                
+        console.log(markup);
+        return markup           
   }
 
 
