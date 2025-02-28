cc9ff6e3cb28c672ad21fcdc5d4bd20f74649e24
Martina-dinapoli98 <martinadinapoli1998@gmail.com>
2025-02-28 12:42:28 +0100

    add main.js

---
+++ main.js
@@ -0,0 +1,41 @@
+const cardEl = document.querySelector('.container .row');
+const cardsUrlEndpoint = 'https://lanciweb.github.io/demo/api/pictures/';
+
+fetch(cardsUrlEndpoint)
+  .then(res => res.json())
+  .then(cards => {
+
+
+    console.log(cards);
+
+    cards.forEach(card =>{
+        let markup = renderCards(card)
+        cardEl.insertAdjacentHTML('beforeend', markup); 
+    
+      })
+
+
+  }).catch(err => console.error(err));
+
+  function renderCards(cards){
+    let markup = `
+     <div class="col-4">
+                    <div class="cards p-3">
+                        <img src="${cards.url}" class="card-img-top" alt="" width="100px" height="200px">
+                        <img src="" alt="" width="20px" class="pin">
+                        <div class="card-body">
+                          <div class="card-text">
+                            ${cards.date}
+                          </div>
+                          <div class="title-img">
+                            ${cards.title}
+                          </div>
+                        </div>
+                      </div>
+                </div>`
+                console.log(markup);
+                return markup
+                
+  }
+
+
